3e58b7c1503d07d78903a85551ca83b18fa33d42
Grantland Hall <grantlandhall@berkeley.edu>
2016-03-14 20:43:13 -0700

    plot psd

--- otz/Calibration.py
+++ otz/Calibration.py
@@ -24,6 +24,10 @@ class Calibration:
             }
         self.rate = self.settings['Sample Rate']
 
+    @property
+    def tdata(self):
+        return np.arange(len(self.ts['x']))/self.rate
+
     def load_settings(self, settings):
         setdict = {}
         with open(settings) as s:
@@ -37,7 +41,7 @@ class Calibration:
         xfig = plt.figure()
         xplt = xfig.add_subplot(111)
         vdat = self.ts['x']
-        xdat = np.arange(len(vdat)) / self.rate
+        xdat = self.tdata
         xplt.plot(xdat, vdat)
         xplt.set_xlabel("Time (s)")
         xplt.set_ylabel("Voltage (V)")
@@ -49,7 +53,7 @@ class Calibration:
         yfig = plt.figure()
         yplt = yfig.add_subplot(111)
         vdat = self.ts['y']
-        ydat = np.arange(len(vdat)) / self.rate
+        ydat = self.tdata
         yplt.plot(ydat, vdat)
         yplt.set_xlabel("Time (s)")
         yplt.set_ylabel("Voltage (V)")
@@ -89,21 +93,32 @@ class Calibration:
         filtered = sig.lfilter(b,a,vdat)
         return filtered
 
-    def plot_band_stop(self, low_f, high_f, axis='x', order=6):
+    def plot_band_stop(self, low_f, high_f, axis='x', order=6, plot_orig=False):
         fig = plt.figure()
         xplt = fig.add_subplot(111)
         vdat = self.ts[axis]
-        tdat = np.arange(len(vdat)) / self.rate
-        xplt.plot(tdat, vdat)
+        if plot_orig:
+            xplt.plot(self.tdata, vdat)
         filtered = self.band_stop(low_f, high_f, axis, order)
-        xplt.plot(tdat, filtered)
+        xplt.plot(self.tdata, filtered)
         xplt.set_xlabel("Time (s)")
         xplt.set_ylabel("Voltage (V)")
         xplt.set_title(
-            "{order}-Order Butterworth Bandstop Filter ({low}Hz-{high}Hz)".format(
+            "Order-{order} Butterworth Bandstop Filter ({low}Hz-{high}Hz)".format(
             order=order,low=low_f,high=high_f))
         fig.show()
     
-    def psd(self, axis='x', vdat=None):
+    def plot_psd(self, axis='x', vdat=None, plot_orig=False):
         if vdat is None:
             vdat = self.ts[axis]
+        f, psd = sig.periodogram(vdat, self.rate)
+        fig = plt.figure()
+        psdplt = fig.add_subplot(111)
+        psdplt.loglog(f, psd, basey=np.e)
+        if plot_orig:
+            f, psd = sig.periodogram(self.ts[axis], self.rate)
+            psdplt.loglog(f, psd)
+        psdplt.set_title("PSD")
+        psdplt.set_xlabel("Frequency ($Hz$)")
+        psdplt.set_ylabel("PSD ($V^2/Hz$)")
+        fig.show()
